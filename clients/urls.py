1e8bd0d51e18726f4c5f2cf0cd7bdef68cfbee3a
Watakaian <ianwataka@gmail.com>
2025-10-01 21:02:06 +0300

    Added Client Detail and Update funtionality

--- clients/urls.py
+++ clients/urls.py
@@ -6,4 +6,6 @@ app_name = "clients"
 urlpatterns = [
     path("", views.client_list, name="list"),
     path("client/create/", views.create_client, name="create"),
+    path("<int:pk>/", views.ClientDetailView.as_view(),name="detail"),
+    path("<int:pk>/edit/", views.ClientUpdateView.as_view(),name="edit"),
 ]
--- clients/views.py
+++ clients/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.views.decorators.csrf import csrf_protect
+from django.views.generic import UpdateView, DetailView, DeleteView
+
+
 
 from clients.models import Client
 from clients.forms import ClientForm
@@ -20,3 +24,18 @@ def create_client(request):
             print("FORM ERRORS", form.errors)
             return redirect("dashboard:index")
     return redirect("dashboard:index")
+
+class ClientDetailView(DetailView):
+    model = Client
+    template_name = "clients/client_detail.html"
+    context_object_name = "client"
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    fields = ["name", "email", "phone_no","company"]
+    template_name = "clients/client_update_form.html"
+    context_object_name = "client"
+
+    def get_success_url(self):
+        return reverse_lazy("clients:detail", kwargs={"pk": self.object.pk})
+
